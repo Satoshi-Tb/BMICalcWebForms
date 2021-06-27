7bae37663a6b8b955875a156771f7339f51ffbf2
Satoshi T <sulp91_fy@yahoo.co.jp>
2021-06-27 22:28:21 +0900

    計算処理、エラーチェック追加

--- BMICalcWebForms/BMICalc.aspx.vb
+++ BMICalcWebForms/BMICalc.aspx.vb
@@ -2,7 +2,8 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        lblBMI.Text = ""
+        lblProperWeight.Text = ""
     End Sub
 
     ''' <summary>
@@ -11,6 +12,27 @@
     ''' <param name="sender"></param>
     ''' <param name="e"></param>
     Protected Sub btnCalc_Click(sender As Object, e As EventArgs)
+        Dim height As Double
+        Dim weight As Double
 
+        If Not Double.TryParse(txtHeight.Text, height) OrElse
+            Not Double.TryParse(txtWeight.Text, weight) OrElse
+            height = 0 OrElse
+            weight = 0 Then
+            ShowMessage("身長、体重には0より大きい数値を入力して下さい")
+            Return
+        End If
+
+        Dim bmi As Double = Math.Round(weight / ((height / 100) ^ 2), 2)
+        Dim stdWeight As Double = Math.Round((height / 100) ^ 2 * 22, 2)
+
+        lblBMI.Text = bmi
+        lblProperWeight.Text = stdWeight & "kg"
     End Sub
+
+    Private Sub ShowMessage(message As String)
+        Dim script As String = $"alert('{message}');"
+        ClientScript.RegisterStartupScript(Me.GetType(), "ALERT_MESSAGE", script, True)
+    End Sub
+
 End Class
